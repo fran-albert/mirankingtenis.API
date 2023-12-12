6122bbc2814f914b25f90666fd545ee5a136fc55
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-12 15:36:11 -0300

    Refactor CategoriesService to use TypeORM repository
    This commit refactors the CategoriesService to use the TypeORM repository for database operations. It also adds error handling for the findOne method and updates the create and findAll methods to use async/await.

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,21 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+  async create(createCategoryDto: CreateCategoryDto) {
+    return await this.categoryRepository.save(createCategoryDto);
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll() {
+    return await this.categoryRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOneBy({ id });
+    if (!category) {
+      throw new BadRequestException('Category not found');
+    }
+    return category;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return await this.categoryRepository.softDelete({ id });
   }
 }
