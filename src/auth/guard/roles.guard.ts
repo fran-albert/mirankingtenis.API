af889a247cba537e0d6192ae773cbe3e8b53528e
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-12 14:51:50 -0300

    Add RolesGuard for role-based authorization

---
+++ src/auth/guard/roles.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorators';
+import { Role } from '../../common/enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (user.roles?.includes(Role.ADMIN)) {
+      return true;
+    }
+
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
