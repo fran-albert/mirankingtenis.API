f8499cf2cba8f760ba33f15564c7a4de1ac64adb
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-12 15:35:45 -0300

    Add Swagger documentation and authentication to CategoriesController

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
+@ApiTags('categories')
+@ApiBearerAuth()
+@Auth(Role.ADMIN)
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
